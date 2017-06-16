1b0eeffee505efb78fd2c697739ad2d3cb6b5c5a
bilibird <1598868798@qq.com>
2017-06-16 11:35:27 +0800

    1.0

---
+++ js/list.js
@@ -0,0 +1,49 @@
+				var music1 = {
+					id:"1",	
+					src:"src/72.mp3",
+					title:"忍者龙剑传3(7-2)",
+					author:"V.A.",
+					img:"http://7xkszy.com2.z0.glb.qiniucdn.com/pics/albums/9603/cover.jpg"
+				};
+				var music2 = {
+					id:"2",
+					src:"src/track7.mp3",
+					title:"森林宝宝丶-track7",
+					author:"森林宝宝丶",
+					img:"http://7xkszy.com2.z0.glb.qiniucdn.com/pics/albums/9604/cover.jpg"
+				};
+				var music3 = {
+					id:"3",
+					src:"src/赤影战士.flac",
+					title:"森林宝宝丶-赤影战士",
+					author:"森林宝宝丶",
+					img:"http://7xkszy.com2.z0.glb.qiniucdn.com/pics/albums/9605/cover.jpg"
+				};
+				var music4 = {
+					id:"4",
+					src:"src/冒险岛4.mp3",
+					title:"高桥名人冒险岛4",
+					author:"森林宝宝丶",
+					img:"http://7xkszy.com2.z0.glb.qiniucdn.com/pics/albums/9605/cover.jpg"
+				};
+				var music5 = {
+					id:"5",
+					src:"src/森林宝宝丶 - 三目童子.mp3",
+					title:"森林宝宝丶 - 三目童子",
+					author:"森林宝宝丶",
+					img:"http://7xkszy.com2.z0.glb.qiniucdn.com/pics/albums/9605/cover.jpg"
+				};
+				var music6 = {
+					id:"6",
+					src:"src/森林宝宝丶 - 雪人兄弟.mp3",
+					title:"森林宝宝丶 - 雪人兄弟",
+					author:"森林宝宝丶",
+					img:"http://7xkszy.com2.z0.glb.qiniucdn.com/pics/albums/9605/cover.jpg"
+				};
+				var arr = [];
+				arr.push(music1);
+				arr.push(music2);
+				arr.push(music3);
+				arr.push(music4);
+				arr.push(music5);
+				arr.push(music6);
